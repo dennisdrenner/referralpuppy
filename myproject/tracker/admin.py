7634fe88dab728653ddd88549935ee97a0fa1f94
Dennis Drenner <dennis@dennisdrenner.com>
2014-04-15 17:57:36 -0400

    modified database models to make use of built in user/auth system

--- myproject/tracker/admin.py
+++ myproject/tracker/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from tracker.models import Users, Reference
 
 # Register your models here.
+
+admin.site.register(Users)
+admin.site.register(Reference)
--- myproject/tracker/models.py
+++ myproject/tracker/models.py
@@ -1,26 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 from django.db import models
 
-class Users(models.Model):
-    joined_date = models.DateTimeField()
-    last_login = models.DateTimeField()
-    first = models.CharField(max_length=20)
-    last = models.CharField(max_length=20)
+class DB_Users(models.Model):
     profession = models.CharField(max_length=20)
-    email = models.CharField(max_length=30)
-    password = models.CharField(max_length=30)
+    referrals_given = models.IntegerField(default=0)
+    referrals_received = models.IntegerField(default=0)
 
+    def __unicode__(self):
+        return self.first + ' ' + self.last
+    
         
 class Reference(models.Model):
     reference_date = models.DateTimeField()
-    referrer = models.ForeignKey(Users, related_name = 'referrer')
-    receiver = models.ForeignKey(Users, related_name = 'receiver')
+    referrer = models.ForeignKey(User, related_name = 'referrer')
+    receiver = models.ForeignKey(User, related_name = 'receiver')
     business_referred = models.CharField(max_length=200)
     reference_note = models.CharField(max_length=200)
     authenticated = models.BooleanField(default=0)
     
-    
+    def __unicode__(self):
+        return self.business_referred
  
